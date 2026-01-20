83c63908df500763e9dacd70a5b8b703f824dc11
markoopapa <markoopapa@ymail.com>
2026-01-20 09:14:04 +0200

    Bump version to 1.2.0 and enhance install/uninstall
    Update version to 1.2.0 and add merchant ID configuration.

--- googlecustomerreviews.php
+++ googlecustomerreviews.php
@@ -9,10 +9,11 @@ class GoogleCustomerReviews extends Module
     {
         $this->name = 'googlecustomerreviews';
         $this->tab = 'front_office_features';
-        $this->version = '1.1.0';
+        $this->version = '1.2.0';
         $this->author = 'markoo';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = ['min' => '1.7.0.0', 'max' => _PS_VERSION_];
+        $this->bootstrap = true; // Fontos az admin felület kinézetéhez
 
         parent::__construct();
 
@@ -22,20 +23,82 @@ class GoogleCustomerReviews extends Module
 
     public function install()
     {
-        return parent::install() && $this->registerHook('displayOrderConfirmation');
+        return parent::install() &&
+            $this->registerHook('displayOrderConfirmation') &&
+            Configuration::updateValue('GCR_MERCHANT_ID', '534853117');
     }
 
     public function uninstall()
     {
-        return parent::uninstall();
+        return parent::uninstall() && Configuration::deleteByName('GCR_MERCHANT_ID');
+    }
+
+    // Admin felület létrehozása (Configure gomb)
+    public function getContent()
+    {
+        $output = '';
+
+        if (Tools::isSubmit('submit' . $this->name)) {
+            $merchant_id = strval(Tools::getValue('GCR_MERCHANT_ID'));
+
+            if (!$merchant_id || empty($merchant_id) || !Validate::isGenericName($merchant_id)) {
+                $output .= $this->displayError($this->l('Invalid Merchant ID'));
+            } else {
+                Configuration::updateValue('GCR_MERCHANT_ID', $merchant_id);
+                $output .= $this->displayConfirmation($this->l('Settings updated successfully'));
+            }
+        }
+
+        return $output . $this->renderForm();
+    }
+
+    protected function renderForm()
+    {
+        $helper = new HelperForm();
+        $helper->show_toolbar = false;
+        $helper->table = $this->table;
+        $helper->module = $this;
+        $helper->default_form_language = $this->context->language->id;
+        $helper->identifier = $this->identifier;
+        $helper->submit_action = 'submit' . $this->name;
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false) . '&configure=' . $this->name . '&tab_module=' . $this->tab . '&module_name=' . $this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+
+        $helper->fields_value['GCR_MERCHANT_ID'] = Configuration::get('GCR_MERCHANT_ID');
+
+        $form = [
+            'form' => [
+                'legend' => [
+                    'title' => $this->l('Settings'),
+                    'icon' => 'icon-cogs',
+                ],
+                'input' => [
+                    [
+                        'type' => 'text',
+                        'label' => $this->l('Google Merchant ID'),
+                        'name' => 'GCR_MERCHANT_ID',
+                        'size' => 20,
+                        'required' => true,
+                        'desc' => $this->l('Enter your 9-digit Google Merchant Center ID.'),
+                    ],
+                ],
+                'submit' => [
+                    'title' => $this->l('Save'),
+                    'class' => 'btn btn-default pull-right',
+                ],
+            ],
+        ];
+
+        return $helper->generateForm([$form]);
     }
 
     public function hookDisplayOrderConfirmation($params)
     {
         $order = $params['order'] ?? null;
-        if (!Validate::isLoadedObject($order)) {
-            return '';
-        }
+        if (!Validate::isLoadedObject($order)) return '';
+
+        $merchant_id = Configuration::get('GCR_MERCHANT_ID');
+        if (!$merchant_id) return '';
 
         $customer = new Customer((int)$order->id_customer);
         $address = new Address((int)$order->id_address_delivery);
@@ -44,15 +107,12 @@ class GoogleCustomerReviews extends Module
         $products = $order->getProducts();
         $gtins = [];
         foreach ($products as $product) {
-            if (!empty($product['ean13'])) {
-                $gtins[] = ['gtin' => $product['ean13']];
-            } elseif (!empty($product['upc'])) {
-                $gtins[] = ['gtin' => $product['upc']];
-            }
+            if (!empty($product['ean13'])) $gtins[] = ['gtin' => $product['ean13']];
+            elseif (!empty($product['upc'])) $gtins[] = ['gtin' => $product['upc']];
         }
 
         $this->context->smarty->assign([
-            'merchant_id' => '534853117',
+            'merchant_id' => $merchant_id,
             'order_id' => (string)$order->id,
             'email' => $customer->email,
             'country_code' => $country->iso_code,
