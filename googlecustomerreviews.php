6271decc176075e36ef9e67b96db731fa33b9584
markoopapa <markoopapa@ymail.com>
2026-01-20 09:26:00 +0200

    Update version and author, add badge support

--- googlecustomerreviews.php
+++ googlecustomerreviews.php
@@ -9,8 +9,8 @@ class GoogleCustomerReviews extends Module
     {
         $this->name = 'googlecustomerreviews';
         $this->tab = 'front_office_features';
-        $this->version = '1.3.1';
-        $this->author = 'markoo';
+        $this->version = '1.4.0';
+        $this->author = 'YourName';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = ['min' => '1.7.0.0', 'max' => _PS_VERSION_];
         $this->bootstrap = true;
@@ -18,65 +18,59 @@ class GoogleCustomerReviews extends Module
         parent::__construct();
 
         $this->displayName = $this->l('Google Customer Reviews');
-        $this->description = $this->l('Google Reviews integration with manual language selection.');
+        $this->description = $this->l('Google Reviews integration with Badge support.');
     }
 
     public function install()
     {
         return parent::install() &&
             $this->registerHook('displayOrderConfirmation') &&
+            $this->registerHook('displayFooter') && // Új hook a jelvénynek
             Configuration::updateValue('GCR_MERCHANT_ID', '534853117') &&
-            Configuration::updateValue('GCR_LANGUAGE', 'hu');
+            Configuration::updateValue('GCR_LANGUAGE', 'hu') &&
+            Configuration::updateValue('GCR_BADGE_POS', 'BOTTOM_RIGHT');
     }
 
     public function uninstall()
     {
         return parent::uninstall() && 
             Configuration::deleteByName('GCR_MERCHANT_ID') &&
-            Configuration::deleteByName('GCR_LANGUAGE');
+            Configuration::deleteByName('GCR_LANGUAGE') &&
+            Configuration::deleteByName('GCR_BADGE_POS');
     }
 
     public function getContent()
     {
-        $output = '';
         if (Tools::isSubmit('submit' . $this->name)) {
             Configuration::updateValue('GCR_MERCHANT_ID', strval(Tools::getValue('GCR_MERCHANT_ID')));
             Configuration::updateValue('GCR_LANGUAGE', strval(Tools::getValue('GCR_LANGUAGE')));
-            $output .= $this->displayConfirmation($this->l('Settings updated successfully'));
+            Configuration::updateValue('GCR_BADGE_POS', strval(Tools::getValue('GCR_BADGE_POS')));
+            return $this->displayConfirmation($this->l('Settings updated successfully'));
         }
-        return $output . $this->renderForm();
+        return $this->renderForm();
     }
 
     protected function renderForm()
     {
-        // Itt definiáljuk a legördülő menü (Drop-down) tartalmát
-        $options = [
-            ['id' => 'hu', 'name' => 'Hungarian (Magyar)'],
-            ['id' => 'ro', 'name' => 'Romanian (Română)'],
-            ['id' => 'en', 'name' => 'English (Angol)'],
+        $lang_options = [
+            ['id' => 'hu', 'name' => 'Hungarian (HU)'],
+            ['id' => 'ro', 'name' => 'Romanian (RO)'],
+            ['id' => 'en', 'name' => 'English (EN)'],
+        ];
+
+        $pos_options = [
+            ['id' => 'BOTTOM_RIGHT', 'name' => 'Bottom Right'],
+            ['id' => 'BOTTOM_LEFT', 'name' => 'Bottom Left'],
+            ['id' => 'USER_DEFINED', 'name' => 'Disabled / Manual'],
         ];
 
         $form = [
             'form' => [
                 'legend' => ['title' => $this->l('Settings'), 'icon' => 'icon-cogs'],
                 'input' => [
-                    [
-                        'type' => 'text',
-                        'label' => $this->l('Google Merchant ID'),
-                        'name' => 'GCR_MERCHANT_ID',
-                        'required' => true,
-                    ],
-                    [
-                        'type' => 'select', // Legördülő menü típus
-                        'label' => $this->l('Survey Language'),
-                        'name' => 'GCR_LANGUAGE',
-                        'options' => [
-                            'query' => $options,
-                            'id' => 'id',
-                            'name' => 'name',
-                        ],
-                        'desc' => $this->l('Select the language for the Google survey popup.'),
-                    ],
+                    ['type' => 'text', 'label' => $this->l('Merchant ID'), 'name' => 'GCR_MERCHANT_ID', 'required' => true],
+                    ['type' => 'select', 'label' => $this->l('Language'), 'name' => 'GCR_LANGUAGE', 'options' => ['query' => $lang_options, 'id' => 'id', 'name' => 'name']],
+                    ['type' => 'select', 'label' => $this->l('Badge Position'), 'name' => 'GCR_BADGE_POS', 'options' => ['query' => $pos_options, 'id' => 'id', 'name' => 'name']],
                 ],
                 'submit' => ['title' => $this->l('Save'), 'class' => 'btn btn-default pull-right'],
             ],
@@ -84,16 +78,26 @@ class GoogleCustomerReviews extends Module
 
         $helper = new HelperForm();
         $helper->module = $this;
-        $helper->name_controller = $this->name;
         $helper->token = Tools::getAdminTokenLite('AdminModules');
         $helper->currentIndex = AdminController::$currentIndex . '&configure=' . $this->name;
-        $helper->default_form_language = (int)Configuration::get('PS_LANG_DEFAULT');
         $helper->fields_value['GCR_MERCHANT_ID'] = Configuration::get('GCR_MERCHANT_ID');
         $helper->fields_value['GCR_LANGUAGE'] = Configuration::get('GCR_LANGUAGE');
+        $helper->fields_value['GCR_BADGE_POS'] = Configuration::get('GCR_BADGE_POS');
 
         return $helper->generateForm([$form]);
     }
 
+    // A JELVÉNY MEGJELENÍTÉSE MINDEN OLDALON
+    public function hookDisplayFooter($params)
+    {
+        $this->context->smarty->assign([
+            'merchant_id' => Configuration::get('GCR_MERCHANT_ID'),
+            'badge_pos' => Configuration::get('GCR_BADGE_POS'),
+        ]);
+        return $this->display(__FILE__, 'views/templates/hook/badge.tpl');
+    }
+
+    // AZ OPT-IN A VISSZAIGAZOLÓ OLDALON
     public function hookDisplayOrderConfirmation($params)
     {
         $order = $params['order'] ?? null;
@@ -101,7 +105,7 @@ class GoogleCustomerReviews extends Module
 
         $this->context->smarty->assign([
             'merchant_id' => Configuration::get('GCR_MERCHANT_ID'),
-            'gcr_lang' => Configuration::get('GCR_LANGUAGE'), // Átadjuk a mentett nyelvet
+            'gcr_lang' => Configuration::get('GCR_LANGUAGE'),
             'order_id' => (string)$order->id,
             'email' => (new Customer((int)$order->id_customer))->email,
             'country_code' => (new Country((int)(new Address((int)$order->id_address_delivery))->id_country))->iso_code,
